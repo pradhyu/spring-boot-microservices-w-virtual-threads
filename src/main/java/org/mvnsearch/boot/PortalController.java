2d0cbd343595e6650f1532ef25f5bccec0ff25a5
PK <pradhyu.shrestha@gmail.com>
2024-10-05 22:42:06 -0400

    Initial commit

---
+++ src/main/java/org/mvnsearch/boot/PortalController.java
@@ -0,0 +1,41 @@
+package org.mvnsearch.boot;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.Callable;
+
+
+@RestController
+public class PortalController {
+
+    @GetMapping("/")
+    public String index() {
+        sayHi("Jackie");
+        return "Hello Virtual Thread!";
+    }
+
+    @GetMapping("/where-am-i")
+    public String getThreadName() {
+        return Thread.currentThread().toString();
+    }
+
+    @GetMapping("/where-am-i-async")
+    public Callable<String> getAsyncThreadName() {
+        return () -> Thread.currentThread().toString();
+    }
+
+    @GetMapping("/reactive")
+    public Mono<String> reactive() {
+        return Mono.just("Hello Reactor!").doOnNext(s -> {
+            System.out.println("Reactive on " + Thread.currentThread());
+        });
+    }
+
+    @Async
+    public void sayHi(String name) {
+        System.out.println("Hi " + name + ", on " + Thread.currentThread());
+    }
+}
