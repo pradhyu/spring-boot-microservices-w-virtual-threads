4b50a3f1602939d7f97abd50bac088e734141c52
PK <pradhyu.shrestha@gmail.com>
2024-10-25 22:54:40 -0400

    Update the scripts

--- src/main/java/org/mvnsearch/boot/PortalController.java
+++ src/main/java/org/mvnsearch/boot/PortalController.java
@@ -2,7 +2,10 @@ package org.mvnsearch.boot;
 
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
 import reactor.core.publisher.Mono;
 
 import java.util.Random;
@@ -14,10 +17,19 @@ public class PortalController {
 
     @GetMapping("/")
     public String index() throws InterruptedException {
-        sayHi("Jackie");
-        int sleepms=new Random().nextInt(10000);
+   
+        int sleepms=new Random().nextInt(100)*100;
         Thread.sleep(sleepms);
-        return "Hello Virtual Thread! " + sleepms+ "ms";
+        sayHi("Jackie - " + sleepms + "ms");
+        return "Hello Virtual Thread! -" + sleepms+ "ms";
+    }
+
+    @GetMapping("/sleep")
+    public String sleep(@RequestParam int ms) throws InterruptedException {
+   
+        Thread.sleep(ms);
+        sayHi("wait sleept for - " + ms + "ms");
+        return "/sleep:Hello Virtual Thread! -" + ms+ "ms";
     }
 
     @GetMapping("/where-am-i")
